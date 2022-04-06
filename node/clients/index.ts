ebcad868f265f81db3bdc6afe281046a9e7e3bd3
PedroHenrique-git <pedromf34@outlook.com>
2022-04-05 22:54:39 -0300

    feat: vtex services

--- node/clients/index.ts
+++ node/clients/index.ts
@@ -1,7 +1,7 @@
-import { IOClients } from '@vtex/api'
-
-
-// Extend the default IOClients implementation with our own custom clients.
+import { IOClients } from '@vtex/api';
+import Analytics from './analytics';
 export class Clients extends IOClients {
-
+    public get analytics() {
+        return this.getOrSet('analytics', Analytics);
+    }
 }
--- node/index.ts
+++ node/index.ts
@@ -1,16 +1,16 @@
 import {
-  LRUCache,
-  Service,
-  ServiceContext,
-  ParamsContext,
-  RecorderState,
-  method,
-} from '@vtex/api'
-import { Clients } from './clients'
-import { analytics } from './handlers/analytics'
+  LRUCache, method, ParamsContext,
+  RecorderState, Service,
+  ServiceContext
+} from '@vtex/api';
+import { Clients } from './clients';
+import { updateLiveUsers } from './event/liveUsersUpdate';
+import { analytics } from './handlers/analytics';
+import productList from './resolvers/product';
 
-// Create a LRU memory cache for the Status client.
-// The @vtex/api HttpClient respects Cache-Control headers and uses the provided cache.
+const THREE_SECONDS_MS = 3 * 1000
+const CONCURRENCY = 10
+
 const memoryCache = new LRUCache<string, any>({ max: 5000 })
 metrics.trackCache('status', memoryCache)
 
@@ -30,6 +30,14 @@ export default new Service<Clients, State, ParamsContext>({
         retries: 2,
         timeout: 10000,
       },
+      events:  {
+        exponentialTimeoutCoefficient: 2,
+        exponentialBackoffCoefficient: 2,
+        initialBackoffDelay: 50,
+        retries: 1,
+        timeout: THREE_SECONDS_MS,
+        concurrency: CONCURRENCY,
+      }
     },
   },
   routes: {
@@ -37,4 +45,14 @@ export default new Service<Clients, State, ParamsContext>({
       GET: [analytics],
     }),
   },
+  events: {
+    liveUserUpdate: updateLiveUsers,
+  },
+  graphql: {
+    resolvers: {
+      Query: {
+        productList
+      }
+    }
+  }
 })
